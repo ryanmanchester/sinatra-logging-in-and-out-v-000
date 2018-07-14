835138a9d79959f74908876a643318a9eb68fe20
Ryan Manchester <ryanm17@gmail.com>
2018-07-14 16:58:45 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,7 +25,10 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
+    if Helpers.is_logged_in?(session)
      erb :account
+   else
+     erb :error
    end
 
   get '/logout' do
