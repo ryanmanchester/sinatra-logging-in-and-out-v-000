a27d57aed82fa633744b48c787ddfdb19b3db981
Ryan Manchester <ryanm17@gmail.com>
2018-07-14 16:55:01 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -3,10 +3,6 @@ class Helpers
 
   def self.current_user(session)
     @user = User.find_by(id: session[:user_id])
-
-    if @user.id == session[:user_id]
-       @user
-     end
    end
 
    def self.is_logged_in?(session)
